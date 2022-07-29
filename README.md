f590820186e9f57c21bef3527f6c59e63b7d0148
Kylez <kylezli@protonmail.com>
2022-07-29 23:02:37 +0800

    add icon in exe; rewrite  logic; fix interupt

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ This exe allows you to record twitch streams live to .mp4 files.
 - Brain
 - Your Love to Namin
 
-## Step
-0) 安裝[Streamlink](https://streamlink.github.io/install.html)  (最新版本就最好，全部按下一步就好了)
+## 安裝步驟
+0) 安裝[Streamlink](https://streamlink.github.io/install.html)  (最新版本就好，全部按下一步就好了)
 
-1) 請確定 `C:\Program Files\Streamlink\ffmpeg\` 有 ffmpeg.exe (正常安裝streamlink就會有了)
+1) 請確定 `C:\Program Files\Streamlink\ffmpeg\` 有 ffmpeg.exe (正常來說安裝streamlink就會有了)
 
 2) 下載我弄的[程式](https://github.com/Kylezhk/twitch-stream-recorder/releases/download/v201/ky1-twitch-recorder-v201.exe) (廢話)
 
@@ -19,15 +19,23 @@ This exe allows you to record twitch streams live to .mp4 files.
 5) 請到這個[網站](https://twitchapps.com/tmi/)拿你自己的Token (按Connect然後複製那段字) (這Token = 無廣告下載VOD) 
 >注: 如果不懂或者擔心這個網站的安全，可以跳過步驟5。~~沒Token也好像沒有廣告~~ 待驗證
 
-6) 開起來！然後貼上你剛剛複製的Token，沒有Token的話就直接按Enter就好了 (第一次會自動生成processed資料夾跟recorded資料夾)
+6) 開起來！程式會先問你要不要選其他的streamer, 如果想下載金主播的話就直接按Enter鍵吧
 
-    程式第一句會像: `Your token:oauth:1234565ep62w5yyh108d52juqq0euul`
+>步驟 6) 例子: `Enter streamer: hellaya1111`
 
-程式會持續顯示出`error: No playable streams found on this URL: twitch.tv/namin1004`來稽查namin有沒有開播
+>注: 第一次會自動生成processed資料夾跟recorded資料夾
 
-金主播下播後然後等程式回到`error: No playable streams found on this URL: twitch.tv/namin1004`, .mp4檔會在processed資料夾出現
+7) 然後程式會問你Token, 你可以複製並貼上你剛剛在5)的步驟Token，沒有Token的話就直接按Enter鍵就好了
 
->注: 當金主播下播後程式會繼續運行24/7，如果不想佔用網絡請關閉程式
+>步驟 7) 例子: `Your token: oauth:1234565ep62w5yyh108d52juqq0euul`
+
+
+
+程式會持續顯示出`namin1004 doesn't streaming now`來稽查namin有沒有開播
+
+金主播下播後然後等程式回到`namin1004 doesn't streaming now`, .mp4檔會在processed資料夾出現
+
+>注: 當金主播下播後程式會繼續運行24/7，如果不想佔用網絡跟電腦資訊請關閉程式
 
 >注: 這是獨立程式，可以跟twitchlink，twitchleecher一起開
 
@@ -40,10 +48,12 @@ This exe allows you to record twitch streams live to .mp4 files.
 - Namin1004 as target streammer
 - Enable low-latency
 - No longer record when hosing
-- Reload attempts increased to 20 times
+- Timeout set to 5 second
+- ffmpeg process will not affect streamlink anymore
 - Applied Twitch authentication typed by user 
 - Delete args in main() for exe purpose only
 - Checking method change to Streamlink rather than using Twitch API
+- Customize your other streamer
 
 If there any bugs, please find me Ky1ez#5989 through the Discord 
 
--- twitch-recorder.py
+++ twitch-recorder.py
@@ -1,182 +1,110 @@
-import datetime
-import glob
 import os
 import subprocess
-import shutil
 import time
-import config
-from threading import Thread
+from multiprocessing import Process, freeze_support
+import datetime as dt
+from glob import glob
+
+clear = lambda: os.system("cls")
 
 
-class TwitchRecorder:
-    def __init__(self):
-        # global configuration
-        self.ffmpeg_path = config.ffmpeg
-        if self.ffmpeg_path == "":
-            self.disable_ffmpeg = True
-        else:
-            self.disable_ffmpeg = False
-        self.root_path = config.root_path
-
-        # user configuration
-        # self.username = config.username
-
-        self.username = "namin1004"
-        self.quality = "best"
-
-        # twitch configuration
-        self.access_token = ""
-
-    def run(self, token):
-        # set token
+def loop_streamlink(token, streamer):
+    while True:
         try:
-            self.access_token = token.split(":")[1]
-        except:
-            pass
-
-        # path to recorded stream
-        recorded_path = os.path.join(self.root_path, "recorded")
-        # path to finished video, errors removed
-        processed_path = os.path.join(self.root_path, "processed")
-
-        # create directory for recordedPath and processedPath if not exist
-        if os.path.isdir(recorded_path) is False:
-            os.makedirs(recorded_path)
-        if os.path.isdir(processed_path) is False:
-            os.makedirs(processed_path)
-
-        # fix videordins from previous recog session
-        try:
-            video_list = [
-                f
-                for f in os.listdir(recorded_path)
-                if os.path.isfile(os.path.join(recorded_path, f))
-            ]
-            if len(video_list) > 0:
-                print("processing previously recorded files")
-            for f in video_list:
-                recorded_filename = os.path.join(recorded_path, f)
-                processed_filename = os.path.join(processed_path, f)
-                self.process_recorded_file(recorded_filename, processed_filename)
-        except Exception as e:
-            print(e)
-
-        print(f"checking for {self.username} and recording with {self.quality} quality")
-        self.loop_check(recorded_path, processed_path)
-
-    def process_recorded_file(self, recorded_filename, processed_filename):
-        if self.disable_ffmpeg:
-            print("moving: ", recorded_filename)
-            shutil.move(recorded_filename, processed_filename)
-        else:
-            print("fixing: ", recorded_filename)
-            self.ffmpeg_copy_and_fix_errors(recorded_filename, processed_filename)
-
-    def ffmpeg_copy_and_fix_errors(self, recorded_filename, processed_filename):
-        try:
-            subprocess.call(
-                [
-                    self.ffmpeg_path,
-                    "-err_detect",
-                    "ignore_err",
-                    "-i",
-                    recorded_filename,
-                    "-c",
-                    "copy",
-                    processed_filename,
-                ],
-                shell=True,
-            )
-            os.remove(recorded_filename)
-        except Exception as e:
-            print(e)
-
-    def loop_check(self, recorded_path, processed_path):
-        while True:
-            filename = datetime.datetime.now().strftime("%Y-%m-%d_%H%M") + ".mp4"
-            recorded_filename = os.path.join(recorded_path, filename)
-            processed_filename = os.path.join(processed_path, filename)
-            if self.access_token != "":
-                # start streamlink process
+            subprocess.check_output(["streamlink", "twitch.tv/" + streamer])
+            if token == None:
+                current_time = dt.datetime.now().strftime("%Y-%m-%d_%H%M")
                 subprocess.call(
                     [
                         "streamlink.exe",
-                        "--hls-playlist-reload-attempts",
-                        "1",
+                        "--hls-timeout",
+                        "5",
                         "--quiet",
-                        f"--twitch-api-header=Authentication=OAuth {self.access_token}",
                         "--twitch-disable-hosting",
                         "--twitch-low-latency",
-                        "twitch.tv/" + self.username,
-                        self.quality,
+                        "twitch.tv/" + streamer,
+                        "best",
                         "-o",
-                        recorded_filename,
+                        "recorded/" + current_time + ".mp4",
                     ],
-                    shell=True,
                 )
             else:
+                current_time = dt.datetime.now().strftime("%Y-%m-%d_%H%M")
                 subprocess.call(
                     [
                         "streamlink.exe",
-                        "--hls-playlist-reload-attempts",
-                        "1",
+                        "--hls-timeout",
+                        "5",
                         "--quiet",
                         "--twitch-disable-hosting",
                         "--twitch-low-latency",
-                        "twitch.tv/" + self.username,
-                        self.quality,
+                        f"--twitch-api-header=Authentication=OAuth {token}",
+                        "twitch.tv/" + streamer,
+                        "best",
                         "-o",
-                        recorded_filename,
+                        "recorded/" + current_time + ".mp4",
                     ],
-                    shell=True,
                 )
+        except subprocess.CalledProcessError:
+            print(f"{streamer} doesn't streaming now")
 
 
-def loop_check_ffmepg():
+def loop_ffmpeg(streamer):
     while True:
-        mp4List = glob.glob("recorded/*.mp4")
-        if mp4List:
-            for mp4 in mp4List:
-                file_name = os.path.basename(mp4)
-                ffmpeg(mp4, f"./processed/{file_name}")
+        try:
+            subprocess.check_output(["streamlink", "twitch.tv/" + streamer])
+            time.sleep(1)
+            pass
+        except:
+            recordedFolder = glob("recorded/*.mp4")
+            if recordedFolder == []:
+                time.sleep(1)
+                pass
+            else:
+                for r in recordedFolder:
+                    subprocess.call(
+                        [
+                            "C:\\Program Files\\Streamlink\\ffmpeg\\ffmpeg.exe",
+                            "-y",
+                            "-err_detect",
+                            "ignore_err",
+                            "-i",
+                            r,
+                            "-c",
+                            "copy",
+                            "processed\\" + r.split("\\")[1],
+                        ],
+                    )
+                    try:
+                        os.remove(r)
+                    except PermissionError:
+                        time.sleep(1)
+                        pass
 
 
-def ffmpeg(recorded_filename, processed_filename):
-    try:
-        subprocess.call(
-            [
-                config.ffmpeg,
-                "-y",
-                "-nostdin",
-                "-err_detect",
-                "ignore_err",
-                "-i",
-                recorded_filename,
-                "-c",
-                "copy",
-                processed_filename,
-            ],
-            shell=True,
-        )
+def main():
+    if os.path.isdir("recorded/") is False:
+        os.makedirs("recorded/")
+    if os.path.isdir("processed\\") is False:
+        os.makedirs("processed\\")
 
-        os.remove(recorded_filename)
-    except Exception as e:
-        print(e)
-        time.sleep(10)
-
-
-def main(token):
-    try:
-        twitch_recorder = TwitchRecorder()
-        t1 = Thread(target=loop_check_ffmepg)
-        t2 = Thread(target=twitch_recorder.run, args=(token,))
-
-        t1.start()
-        t2.start()
-    finally:
-        print("exiting")
+    print(
+        "Defualt streamer is namin1004, if you want to change, please input another streamer name. Otherise, press enter to continue."
+    )
+    streamer = str(input("Enter streamer: ") or "namin1004")
+    clear()
+    token = input("Your token: ")
+    clear()
+    Process(
+        target=loop_streamlink,
+        args=(
+            token,
+            streamer,
+        ),
+    ).start()
+    Process(target=loop_ffmpeg, args=(streamer,)).start()
 
 
 if __name__ == "__main__":
-    token = input("Your token:")
-    main(token)
+    freeze_support()
+    main()
